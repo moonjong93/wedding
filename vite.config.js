46dc57b8700a4e43398d0265f231ece92106e388
Moonjongmin <44762611+CEOmurky@users.noreply.github.com>
2022-10-13 22:19:24 +0900

    base 추가

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 export default {
-  build: {
-    outDir: './docs', // this line place index.html in the public folder
-  }
+    base: '/docs/',
+    build: {
+        outDir: './docs', // this line place index.html in the public folder
+    }
 }
